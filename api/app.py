acf593c982519330e782c042dc46bfc7da2b239a
Emma Wilson <emma.wilson@indexexchange.com>
2020-11-23 17:48:17 -0500

    backend business

--- api/app.py
+++ api/app.py
@@ -1,12 +1,28 @@
 from flask import Flask
 import os
+import threading
+import time
+from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 
+# This should be run in the terminal: 
+# export SLACK_BOT_TOKEN='xoxb-1522067689266-1519140730133-eufROWvsyLgZWMltdL7NUwDR'
+SLACK_SIGNING_SECRET = '9abe82c0eb94caca58b43957653142d6'
+SLACK_BOT_TOKEN="xoxb-1522067689266-1519140730133-eufROWvsyLgZWMltdL7NUwDR"
+BOT_NAME = "HogBot"
+
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events")
+
+# Create an event listener for "reaction_added" events and print the emoji name
+@slack_events_adapter.on("reaction_added")
+def reaction_added(event_data):
+  emoji = event_data["event"]["reaction"]
+  print(emoji)
+
 @app.route("/")
 def home():
-    return "Welcome to Hogwarts 🧙"
-
+    return "Welcome to Hogwarts"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
