f4b93c1057710f12338bd10f77af8d9d785d2999
Cameron Ouellette <cameron.ouellette@indexexchange.com>
2020-11-23 18:24:25 -0500

    add team model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import db
 
+class Team(db.Model):
+    teamID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, 100)
+
 class User(db.Model):
     userID = db.Column(db.Integer, primary_key=True)
     teamID = db.Column(db.Integer, db.ForeignKey('teams.teamID'))
