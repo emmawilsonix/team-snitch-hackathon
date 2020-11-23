b13646deb97edc6c8c91a9a47b495a5ef1784d38
Joshua Butcher <joshua.butcher@indexexchange.com>
2020-11-23 17:06:54 -0500

    Fix column def with User

--- api/models.py
+++ api/models.py
@@ -4,4 +4,4 @@ from app import db
 class User(db.Model):
     userID = db.Column(db.Integer, primary_key=True)
     teamID = db.Column(db.Integer, db.ForeignKey('teams.teamID'))
-    emailAddress = db.Column(userID)
+    emailAddress = db.Column(db.String, 100)
