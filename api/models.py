de2fa97dfcbe32458ba295a497b1043edfd2e117
Cameron Ouellette <cameron.ouellette@indexexchange.com>
2020-11-23 22:39:42 -0500

    update db models

--- api/models.py
+++ api/models.py
@@ -2,10 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from app import db
 
 class Team(db.Model):
-    teamID = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, 100)
+    teamID = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), unique=True, nullable=False)
 
 class User(db.Model):
-    userID = db.Column(db.Integer, primary_key=True)
-    teamID = db.Column(db.Integer, db.ForeignKey('teams.teamID'))
-    emailAddress = db.Column(db.String, 100)
+    userID = db.Column(db.Integer, primary_key=True, nullable=False)
+    teamID = db.Column(db.Integer, db.ForeignKey('teams.teamID'), nullable=False)
+    emailAddress = db.Column(db.String(100), unique=True, nullable=False)
